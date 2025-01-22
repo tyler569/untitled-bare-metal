4db15fbab171b9bcfc32ff04fc15eec118b58ccd
Tyler Philbrick <git@choam.me>
2025-01-21 19:19:44 -0800

    Page table maker

--- user/exec.c
+++ user/exec.c
@@ -10,6 +10,7 @@
 
 static inline struct elf_phdr *get_phdr (struct elf_ehdr *, size_t);
 cptr_t allocate_pages (cptr_t untyped, size_t);
+cptr_t create_page_tables (cptr_t untyped, uintptr_t min_address, uintptr_t max_address);
 
 int
 create_process (void *elf_data, size_t elf_size, cptr_t untyped, cptr_t *tcb, cptr_t *cspace)
@@ -38,14 +39,14 @@ create_process (void *elf_data, size_t elf_size, cptr_t untyped, cptr_t *tcb, cp
           uintptr_t fstart = phdr->offset;
           uintptr_t fend = fstart + phdr->filesz;
 
-          printf ("want to load %010lx-%010lx to %016lx-%016lx\n", fstart,
-                  fend, start, end);
-
           if (lowest_addr == 0 || start < lowest_addr)
             lowest_addr = start;
           if (end > highest_addr)
             highest_addr = end;
           needed_pages += (end - start + 0xfff) / 0x1000;
+
+          printf ("want to load %010lx-%010lx to %016lx-%016lx, needs %zu pages\n", fstart,
+                  fend, start, end, (end - start + 0xfff) / 0x1000);
         }
     }
 
@@ -66,41 +67,45 @@ get_phdr (struct elf_ehdr *ehdr, size_t i)
 }
 
 cptr_t
-allocate_pages (cptr_t untyped, size_t n)
+allocate (cptr_t untyped, word_t type, size_t n)
 {
   cptr_t cptr = cptr_alloc_range (n);
-  untyped_retype (untyped, cap_x86_64_page, 12, init_cap_root_cnode, init_cap_root_cnode, 64, cptr, n);
+  untyped_retype (untyped, type, 0, init_cap_root_cnode, init_cap_root_cnode, 64, cptr, n);
   return cptr;
 }
 
-cptr_t
-allocate_pml4 (cptr_t untyped)
-{
-  cptr_t cptr = cptr_alloc_range (1);
-  untyped_retype (untyped, cap_x86_64_pml4, 0, init_cap_root_cnode, init_cap_root_cnode, 64, cptr, 1);
-  return cptr;
-}
+#define allocate_pages(untyped, n) allocate(untyped, cap_x86_64_page_table, n)
+#define allocate_pml4(untyped) allocate(untyped, cap_x86_64_pml4, 1)
+#define allocate_pdpt(untyped) allocate(untyped, cap_x86_64_pdpt, 1)
+#define allocate_pd(untyped) allocate(untyped, cap_x86_64_pd, 1)
+#define allocate_pt(untyped) allocate(untyped, cap_x86_64_pt, 1)
+
+constexpr uintptr_t page_size = 0x1000;
+constexpr uintptr_t page_table_size = page_size * 512;
+constexpr uintptr_t page_directory_size = page_table_size * 512;
 
 cptr_t
-allocate_pdpt (cptr_t untyped)
+create_page_tables (cptr_t untyped, uintptr_t min_address, uintptr_t max_address)
 {
-  cptr_t cptr = cptr_alloc_range (1);
-  untyped_retype (untyped, cap_x86_64_pdpt, 0, init_cap_root_cnode, init_cap_root_cnode, 64, cptr, 1);
-  return cptr;
-}
+  min_address &= page_directory_size - 1;
+  max_address = (max_address + page_directory_size - 1) & ~(page_directory_size - 1);
 
-cptr_t
-allocate_pd (cptr_t untyped)
-{
-  cptr_t cptr = cptr_alloc_range (1);
-  untyped_retype (untyped, cap_x86_64_pd, 0, init_cap_root_cnode, init_cap_root_cnode, 64, cptr, 1);
-  return cptr;
-}
+  // assert (max_address > min_address);
+  // assert (max_address < page_directory_size);
 
-cptr_t
-allocate_pt (cptr_t untyped)
-{
-  cptr_t cptr = cptr_alloc_range (1);
-  untyped_retype (untyped, cap_x86_64_pt, 0, init_cap_root_cnode, init_cap_root_cnode, 64, cptr, 1);
-  return cptr;
+  cptr_t pml4 = allocate_pml4 (untyped);
+
+  cptr_t pdpt = allocate_pdpt (untyped);
+  x86_64_pdpt_map (pml4, pdpt, 0, 0);
+
+  cptr_t pd = allocate_pd (untyped);
+  x86_64_pd_map (pml4, pd, 0, 0);
+
+  for (uintptr_t i = min_address; i < max_address; i += 0x200000)
+    {
+      cptr_t pt = allocate_pt (untyped);
+      x86_64_pd_map (pd, pt, i, 0);
+    }
+
+  return pml4;
 }
