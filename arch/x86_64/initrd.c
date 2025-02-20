6f73dddda572c460f272834267048dc869a718de
Tyler Philbrick <git@choam.me>
2025-02-19 22:34:49 -0800

    Move get_initrd back to arch, could be different

--- arch/x86_64/initrd.c
+++ arch/x86_64/initrd.c
@@ -3,3 +3,20 @@
 #include "stddef.h"
 #include "stdio.h"
 
+static struct limine_module_request moduleinfo = {
+  .id = LIMINE_MODULE_REQUEST,
+};
+
+bool
+get_initrd_info (void **initrd_start, size_t *initrd_size)
+{
+  struct limine_module_response *resp = volatile_read (moduleinfo.response);
+
+  if (resp->module_count == 0)
+    return false;
+
+  *initrd_start = (void *)resp->modules[0]->address;
+  *initrd_size = resp->modules[0]->size;
+
+  return true;
+}
--- kern/boot.c
+++ kern/boot.c
@@ -11,24 +11,6 @@
 struct tcb init_tcb;
 cte_t init_cnode[BIT (INIT_CNODE_SIZE_BITS)];
 
-static struct limine_module_request moduleinfo = {
-  .id = LIMINE_MODULE_REQUEST,
-};
-
-bool
-get_initrd_info (void **initrd_start, size_t *initrd_size)
-{
-  struct limine_module_response *resp = volatile_read (moduleinfo.response);
-
-  if (resp->module_count == 0)
-    return false;
-
-  *initrd_start = (void *)resp->modules[0]->address;
-  *initrd_size = resp->modules[0]->size;
-
-  return true;
-}
-
 static struct limine_framebuffer_request fbinfo = {
   .id = LIMINE_FRAMEBUFFER_REQUEST,
 };
