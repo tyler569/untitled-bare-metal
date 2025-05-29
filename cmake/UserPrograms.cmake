8da3650f9cad489593fb8850d63d2c7f41caacf9
Tyler Philbrick <tphil@meraki.com>
2025-05-29 10:00:12 -0700

    Refactor user cmake files

--- cmake/UserPrograms.cmake
+++ cmake/UserPrograms.cmake
@@ -14,7 +14,7 @@ if (NOT TARGET userlib)
 endif ()
 
 function(add_single_file_program name)
-  add_executable(${name} user/${name}.c)
+  add_executable(${name} ${name}.c)
   target_link_libraries(${name} PRIVATE userlib)
 
   define_file_basename_for_sources(${name})
@@ -22,7 +22,6 @@ function(add_single_file_program name)
 endfunction()
 
 function(add_subdirectory_program name)
-  include_directories(user)
-  add_subdirectory(user/${name})
+  add_subdirectory(${name})
   set(archive_binaries ${archive_binaries} $<TARGET_FILE:${name}> PARENT_SCOPE)
 endfunction()
